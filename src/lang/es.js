7a0e21b14436c0c723ffc407f86cd98f90720861
magdielikari <magdielikari@gmail.com>
2018-01-14 11:54:52 -0500

    Boostrap commit, waitress and cashier profile

---
+++ src/lang/es.js
@@ -0,0 +1,10 @@
+const lang = {
+  contact: 'Contacto',
+  experience: 'Experiencia',
+  education: 'Educación',
+  skills: 'Habilidades',
+  about: 'Sobre mí',
+  hobbies: 'Intereses',
+  attitudes: 'Actitudes'
+};
+export default lang;
